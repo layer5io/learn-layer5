6c1cb2fed57bbdf34d3b1bbc2d6bc0030ef0c513
kanishkarj <kanishkarj@hotmail.com>
2020-06-04 14:44:52 +0530

    call route Ready
    Signed-off-by: kanishkarj <kanishkarj@hotmail.com>

--- service/main.go
+++ service/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func MetricsMiddleware(next http.Handler) http.Handler {
@@ -14,13 +14,54 @@ func MetricsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func call(w http.ResponseWriter, req *http.Request) {}
-func getMetrics(w http.ResponseWriter, req *http.Request) {}
+func call(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Method not defined", http.StatusBadRequest)
+		return
+	}
+
+	defer req.Body.Close()
+
+	var data map[string]string
+	bytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "Error reading body", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		http.Error(w, "Error parsing body", http.StatusBadRequest)
+		return
+	}
+
+	host := data["host"]
+	body := data["body"]
+
+	resp, err := http.Post(host, "application/json", strings.NewReader(body))
+	if err != nil {
+		fmt.Printf("%v", err)
+		http.Error(w, "Error parsing response body", http.StatusBadRequest)
+		return
+	}
+
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		w.WriteHeader(http.StatusOK)
+	}
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	w.Write(bytes)
+}
+
+func getMetrics(w http.ResponseWriter, req *http.Request)     {}
 func refreshMetrics(w http.ResponseWriter, req *http.Request) {}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/call", MetricsMiddleware(http.HandlerFunc(hello)))
+	mux.Handle("/call", MetricsMiddleware(http.HandlerFunc(call)))
 	mux.Handle("/metrics/get", http.HandlerFunc(getMetrics))
 	mux.Handle("/metrics/refresh", http.HandlerFunc(refreshMetrics))
 	http.ListenAndServe(":9091", mux)
