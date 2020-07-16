1047cb8e042fa67657bd866c20c695bbe6250029
kanishkarj <kanishkarj@hotmail.com>
2020-07-16 10:14:52 +0530

    All tests for traffic split
    Signed-off-by: kanishkarj <kanishkarj@hotmail.com>

--- smi-conformance/test-gen/traffic-split.go
+++ smi-conformance/test-gen/traffic-split.go
@@ -11,10 +11,16 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const reqNo = 20
+
 func (smi *SMIConformance) TrafficSplitGetTests() map[string]test.CustomTest {
 	testHandlers := make(map[string]test.CustomTest)
 
 	testHandlers["trafficDefault"] = smi.trafficSplitDefault
+	testHandlers["trafficOnlyB"] = smi.trafficSplitOnlyB
+	testHandlers["trafficOnlyC"] = smi.trafficSplitOnlyC
+	testHandlers["trafficBGrtC"] = smi.trafficSplitBGrtC
+	testHandlers["trafficCGrtB"] = smi.trafficSplitCGrtB
 
 	return testHandlers
 }
@@ -26,7 +32,6 @@ func (smi *SMIConformance) trafficSplitDefault(
 	DiscoveryClient func() (discovery.DiscoveryInterface, error),
 	Logger testutils.Logger,
 ) []error {
-	Logger.Log("Service sdfsdfsdf")
 	time.Sleep(5 * time.Second)
 	namespace = "kuttl-test-stage"
 	kubeClient, err := clientFn(false)
@@ -70,6 +75,226 @@ func (smi *SMIConformance) trafficSplitDefault(
 		return nil
 	}
 	Logger.Log("Validated: Random Request count")
+
+	Logger.Log("Done")
+	return nil
+}
+
+func (smi *SMIConformance) trafficSplitOnlyB(
+	t *testing.T,
+	namespace string,
+	clientFn func(forceNew bool) (client.Client, error),
+	DiscoveryClient func() (discovery.DiscoveryInterface, error),
+	Logger testutils.Logger,
+) []error {
+	time.Sleep(5 * time.Second)
+	namespace = "kuttl-test-stage"
+	kubeClient, err := clientFn(false)
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	clusterIPs, err := GetClusterIPs(kubeClient, namespace)
+
+	ClearMetrics(clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort())
+	ClearMetrics(clusterIPs[SERVICE_B_NAME], smi.SMObj.SvcBGetPort())
+	ClearMetrics(clusterIPs[SERVICE_C_NAME], smi.SMObj.SvcCGetPort())
+
+	// Generate traffic to the traffic split service
+	svcTrafficSplit := fmt.Sprintf("http://app-svc.%s.svc.cluster.local.:9091/%s", namespace, ECHO)
+	jsonStr := []byte(`{"url":"` + svcTrafficSplit + `", "body":"", "method": "GET", "headers": {}}`)
+
+	url := fmt.Sprintf("http://%s:%s/%s", clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort(), CALL)
+
+	if err = generatePOSTLoad(reqNo, url, jsonStr); err != nil {
+		t.Fail()
+		return []error{err}
+	}
+
+	metricsSvcB, err := GetMetrics(clusterIPs[SERVICE_B_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service B : Requests Recieved", metricsSvcB.ReqReceived)
+
+	metricsSvcC, err := GetMetrics(clusterIPs[SERVICE_C_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service C : Requests Recieved", metricsSvcC.ReqReceived)
+
+	if !(len(metricsSvcB.ReqReceived) == reqNo && len(metricsSvcC.ReqReceived) == 0) {
+		t.Fail()
+		return nil
+	}
+	Logger.Log("Validated: C Request count zero")
+
+	Logger.Log("Done")
+	return nil
+}
+
+func (smi *SMIConformance) trafficSplitOnlyC(
+	t *testing.T,
+	namespace string,
+	clientFn func(forceNew bool) (client.Client, error),
+	DiscoveryClient func() (discovery.DiscoveryInterface, error),
+	Logger testutils.Logger,
+) []error {
+	time.Sleep(5 * time.Second)
+	namespace = "kuttl-test-stage"
+	kubeClient, err := clientFn(false)
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	clusterIPs, err := GetClusterIPs(kubeClient, namespace)
+
+	ClearMetrics(clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort())
+	ClearMetrics(clusterIPs[SERVICE_B_NAME], smi.SMObj.SvcBGetPort())
+	ClearMetrics(clusterIPs[SERVICE_C_NAME], smi.SMObj.SvcCGetPort())
+
+	// Generate traffic to the traffic split service
+	svcTrafficSplit := fmt.Sprintf("http://app-svc.%s.svc.cluster.local.:9091/%s", namespace, ECHO)
+	jsonStr := []byte(`{"url":"` + svcTrafficSplit + `", "body":"", "method": "GET", "headers": {}}`)
+
+	url := fmt.Sprintf("http://%s:%s/%s", clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort(), CALL)
+
+	if err = generatePOSTLoad(reqNo, url, jsonStr); err != nil {
+		t.Fail()
+		return []error{err}
+	}
+
+	metricsSvcB, err := GetMetrics(clusterIPs[SERVICE_B_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service B : Requests Recieved", metricsSvcB.ReqReceived)
+
+	metricsSvcC, err := GetMetrics(clusterIPs[SERVICE_C_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service C : Requests Recieved", metricsSvcC.ReqReceived)
+
+	if !(len(metricsSvcB.ReqReceived) == 0 && len(metricsSvcC.ReqReceived) == reqNo) {
+		t.Fail()
+		return nil
+	}
+	Logger.Log("Validated: B Request count zero")
+
+	Logger.Log("Done")
+	return nil
+}
+
+func (smi *SMIConformance) trafficSplitBGrtC(
+	t *testing.T,
+	namespace string,
+	clientFn func(forceNew bool) (client.Client, error),
+	DiscoveryClient func() (discovery.DiscoveryInterface, error),
+	Logger testutils.Logger,
+) []error {
+	time.Sleep(5 * time.Second)
+	namespace = "kuttl-test-stage"
+	kubeClient, err := clientFn(false)
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	clusterIPs, err := GetClusterIPs(kubeClient, namespace)
+
+	ClearMetrics(clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort())
+	ClearMetrics(clusterIPs[SERVICE_B_NAME], smi.SMObj.SvcBGetPort())
+	ClearMetrics(clusterIPs[SERVICE_C_NAME], smi.SMObj.SvcCGetPort())
+
+	// Generate traffic to the traffic split service
+	svcTrafficSplit := fmt.Sprintf("http://app-svc.%s.svc.cluster.local.:9091/%s", namespace, ECHO)
+	jsonStr := []byte(`{"url":"` + svcTrafficSplit + `", "body":"", "method": "GET", "headers": {}}`)
+
+	url := fmt.Sprintf("http://%s:%s/%s", clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort(), CALL)
+
+	if err = generatePOSTLoad(reqNo, url, jsonStr); err != nil {
+		t.Fail()
+		return []error{err}
+	}
+
+	metricsSvcB, err := GetMetrics(clusterIPs[SERVICE_B_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service B : Requests Recieved", metricsSvcB.ReqReceived)
+
+	metricsSvcC, err := GetMetrics(clusterIPs[SERVICE_C_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service C : Requests Recieved", metricsSvcC.ReqReceived)
+
+	if !(len(metricsSvcB.ReqReceived) > len(metricsSvcC.ReqReceived)) {
+		t.Fail()
+		return nil
+	}
+	Logger.Log("Validated: B Request count greater than C")
+
+	Logger.Log("Done")
+	return nil
+}
+
+func (smi *SMIConformance) trafficSplitCGrtB(
+	t *testing.T,
+	namespace string,
+	clientFn func(forceNew bool) (client.Client, error),
+	DiscoveryClient func() (discovery.DiscoveryInterface, error),
+	Logger testutils.Logger,
+) []error {
+	time.Sleep(5 * time.Second)
+	namespace = "kuttl-test-stage"
+	kubeClient, err := clientFn(false)
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	clusterIPs, err := GetClusterIPs(kubeClient, namespace)
+
+	ClearMetrics(clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort())
+	ClearMetrics(clusterIPs[SERVICE_B_NAME], smi.SMObj.SvcBGetPort())
+	ClearMetrics(clusterIPs[SERVICE_C_NAME], smi.SMObj.SvcCGetPort())
+
+	// Generate traffic to the traffic split service
+	svcTrafficSplit := fmt.Sprintf("http://app-svc.%s.svc.cluster.local.:9091/%s", namespace, ECHO)
+	jsonStr := []byte(`{"url":"` + svcTrafficSplit + `", "body":"", "method": "GET", "headers": {}}`)
+
+	url := fmt.Sprintf("http://%s:%s/%s", clusterIPs[SERVICE_A_NAME], smi.SMObj.SvcAGetPort(), CALL)
+
+	if err = generatePOSTLoad(reqNo, url, jsonStr); err != nil {
+		t.Fail()
+		return []error{err}
+	}
+
+	metricsSvcB, err := GetMetrics(clusterIPs[SERVICE_B_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service B : Requests Recieved", metricsSvcB.ReqReceived)
+
+	metricsSvcC, err := GetMetrics(clusterIPs[SERVICE_C_NAME], "9091")
+	if err != nil {
+		t.Fail()
+		return []error{err}
+	}
+	Logger.Log("Service C : Requests Recieved", metricsSvcC.ReqReceived)
+
+	if !(len(metricsSvcB.ReqReceived) < len(metricsSvcC.ReqReceived)) {
+		t.Fail()
+		return nil
+	}
+	Logger.Log("Validated: C Request count greater than B")
 
 	Logger.Log("Done")
 	return nil
