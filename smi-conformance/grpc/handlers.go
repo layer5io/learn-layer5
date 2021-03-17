92d8cea8af403e5106788aa43acfd012d93067d7
dhruv0000 <patel.4@iitj.ac.in>
2021-03-17 12:54:36 +0530

    Minor changes
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- smi-conformance/grpc/handlers.go
+++ smi-conformance/grpc/handlers.go
@@ -99,12 +99,14 @@ func (s *Service) RunTest(ctx context.Context, req *conformance.Request) (*confo
 			// A hacky way to see the testStep Failed, since KUDO only provides it in Failure.Message
 			re := regexp.MustCompile(`[0-9]+`)
 			stepFailed := re.FindAllString(res.Failure.Message, 1)
-			passed, _ := strconv.Atoi(stepFailed[0])
-			passed = passed - 1
-			failures := stepsCount[res.Name] - passed
-			totalFailures += failures
-			if (passed) >= (stepsCount[res.Name] / 2) {
-				d.Capability = conformance.Capability_HALF
+			if len(stepFailed) != 0 {
+				passed, _ := strconv.Atoi(stepFailed[0])
+				passed = passed - 1
+				failures := stepsCount[res.Name] - passed
+				totalFailures += failures
+				if (passed) >= (stepsCount[res.Name] / 2) {
+					d.Capability = conformance.Capability_HALF
+				}
 			}
 		}
 		details = append(details, d)
