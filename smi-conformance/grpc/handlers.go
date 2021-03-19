5088dfab39f2969ddbc0c6f94a4a17cce190b19b
dhruv0000 <patel.4@iitj.ac.in>
2021-03-19 14:36:35 +0530

    Docker Build Fix
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- smi-conformance/grpc/handlers.go
+++ smi-conformance/grpc/handlers.go
@@ -105,7 +105,7 @@ func (s *Service) RunTest(ctx context.Context, req *conformance.Request) (*confo
 
 			// A hacky way to see the testStep Failed, since KUDO only provides it in Failure.Message
 			re := regexp.MustCompile(`[0-9]+`)
-			if res.Failure != nil {
+			if res.Failure.Message != "" {
 				stepFailed := re.FindAllString(res.Failure.Message, 1)
 				if len(stepFailed) != 0 {
 					passed, _ := strconv.Atoi(stepFailed[0])
